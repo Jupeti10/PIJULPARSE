d3479ff9cdbe32f59201fb33ac08e319b3c5197d
uusi <uusi@Mikko-MBP.zyxel.com>
2015-10-08 21:38:53 +0300

    Read file and calculate hash

--- demo.sh
+++ demo.sh
@@ -4,18 +4,20 @@
 # - tiedoston nimi
 # - int
 # - flag
-# Lue tiedosto
-# Laske hash?
 
-hash="yourpassword" | openssl sha1 | awk '{$2}'
-# echo $hash
-echo "Hash: $hash"
+FILE_NAME="file.txt"
+flag=false
+int=3
+
+
+FILE_CONTENT="`cat $FILE_NAME`"
+
+hash=`openssl dgst -sha1 $FILE_NAME | sed 's/^.*= //'`
+
 
 PARSE_APP_ID=t1Hg3vjypwZsBrc5zAg1yXCfoHtkqN7brnzSm2vq
 PARSE_REST_KEY=pKuNTc0sWK5AgsgwjTdWJE8SSV2bdwgTIErv9Fx7
 
-FILE_NAME="hallo.txt"
-FILE_CONTENT="Hallo there!"
 
 url="https://api.parse.com/1/files/$FILE_NAME"
 regexp='"name":"(.*)",'
@@ -32,9 +34,7 @@ fi
 file_name=$name
 echo "File added: $file_name"
 
-flag=false
-hash="e9782b17b7eebae551c223fc243c811d39fc31fd"
-int=3
+
 
 curl -X POST -H "X-Parse-Application-Id: $PARSE_APP_ID" -H "X-Parse-REST-API-Key: $PARSE_REST_KEY" -H "Content-Type: application/json" \
   https://api.parse.com/1/classes/Patch \
